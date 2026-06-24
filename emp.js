e98c4d27ea783fa72fab61b142bf7202103cbbbc
Shiva6301 <134802904+Shiva6301@users.noreply.github.com>
2026-06-24 13:18:08 +0530

    Add files via upload

---
+++ emp.js
@@ -0,0 +1,72 @@
+/*let a = prompt ("whats your name");
+console.log(a);
+document.writeln(a);*/
+
+// task 2//
+
+/*let b = confirm("Are ready to join todays training of j.s");
+console.log(b);*/
+
+// task 3 //
+
+/*let greet = alert("welcome TO The stacly solutions");
+console.log("customer entered the website");*/
+
+// task 4 //
+ 
+/*let n = prompt(" enter your name");
+ let a = prompt("enter your age");
+ console.log("sudent name:", n);
+ console.log("student age:", a);*/
+
+ // task 5 //
+
+ /*var x = 100;
+ var x = 150;
+ console.log( "updated price:", x);*/
+
+
+ // task 6 //
+
+/*let user = prompt("enter user name");
+if (user === "" || user === null) {
+    console.warn("user name should not be empty");
+} else {
+    console.log("user name:", user);
+}*/
+
+// Task 7 //
+
+/*let x = alert("website under maintainance");
+console.error("website is currently unavaialable");*/
+
+// Task 8 //
+
+/*let feedback = prompt("how was javascript session today");
+console.log(feedback);
+document.writeln("thank you for your feedback");*/
+
+// Task 9 //
+
+/*let user = prompt("name");
+let city = prompt("city name");
+let lang = prompt("fav programming lang");
+
+ console.log(user);
+ console.log(city);
+ console.log(lang);*/
+
+
+ // Task 10 //
+
+ let name = prompt("full name");
+ let mail = prompt("E-mail");
+ let number = prompt ("mobile number");
+
+ console.log( "===== Regristation details =====");
+ console.log("full name:",name);
+ console.log("Email:",mail);
+ console.log("mobile number:", number);
+ alert("registration sucessfull");
+
+
